0c430d930dcf2a00dc9ead0a1e908de4699d6426
YaninaKravchenko <yakrauchanka@gmail.com>
2022-02-10 15:16:42 +0300

    Done

--- src/index.js
+++ src/index.js
@@ -38,9 +38,49 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    // let num = (expr.length/10);
+    //console.log(expr.match(/.{1,10}/g));
+    
+    let arr = [];
+    for (let i = 0; i < expr.length; i += 10) {
+     arr.push(expr.substring(i, i+10));
+    }
+    //console.log(arr);
+    
+    let arrStr = [];
+    //let el = arr[i];
+    //let str = '';
+    for (let i = 0; i < arr.length; i++) {
+      let str = '';
+        if (arr[i] == '**********') {
+          str += ' ';
+        }
+        else {
+           for (let k = 0; k < arr[i].length; k+=2) {
+            if (arr[i].slice(k,k+2) == 10) {
+              str += '.';
+            }
+            else
+              if (arr[i].slice(k,k+2) == 11) {
+              str += '-';
+             }
+            else
+             if (arr[i].slice(k,k+2) == 00) {
+             str += '';
+             }
+         }
+      }
+      if (str == ' ') {
+        arrStr.push(str);
+      } 
+      else
+     arrStr.push(MORSE_TABLE[str]);
+    }  
+    //console.log(arrStr.join(''));
+  return arrStr.join('');
+  }
 
 module.exports = {
-    decode
+  decode
 }
+   
